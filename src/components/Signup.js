d2e27b18845fc51e95124688e966d2a9bc7d59d0
Vajihe Ahmadi <vajiheahmadi78@gmail.com>
2023-05-11 21:12:24 +0430

    validation

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,6 @@
-import React,{useState} from 'react';
+import React,{useState , useEffect} from 'react';
+//function
+import { validate } from './validate';
 
 const Signup = () => {
     const [data,setData]=useState({
@@ -8,6 +10,15 @@ const Signup = () => {
         confirmPassword:"",
         isAccepted:false,
     })
+
+    const [errors,setErrors]=useState({});
+
+    const [touched,setTouched]=useState({});
+
+    useEffect(()=>{
+        setErrors(validate(data))
+    },[data,touched])
+
     const changeHandler=(event)=>{
         if(event.target.name === "isAccepted"){
             setData({...data,[event.target.name]:event.target.checkd})
@@ -16,34 +27,47 @@ const Signup = () => {
             setData({...data,[event.target.name]:event.target.value})
         }
     }
+
+    const focusHandler=(event)=>{
+        setTouched({...touched,[event.target.name]:true})
+    }
+
+    const submitHandler=()=>{
+        
+    }
     return (
-        <div>
+        <form onSubmit={submitHandler}>
             <h1>Signup</h1>
             <div>
                 <label>Name</label>
-                <input type="text" name="name" value={data.name}  onChange={changeHandler}/>
+                <input type="text" name="name" value={data.name}  onChange={changeHandler} onFocus={focusHandler}/>
+                {errors.name && touched.name && <span>{errors.name}</span>}
             </div>
             <div>
                 <label>email</label>
-                <input type="text" name="email" value={data.email}  onChange={changeHandler}/>
+                <input type="text" name="email" value={data.email}  onChange={changeHandler} onFocus={focusHandler}/>
+                {errors.email && touched.email && <span>{errors.email}</span>}
             </div>
             <div>
                 <label>Password</label>
-                <input type="password" name="password" value={data.password} onChange={changeHandler} />
+                <input type="password" name="password" value={data.password} onChange={changeHandler} onFocus={focusHandler} />
+                {errors.password && touched.password && <span>{errors.password}</span>}
             </div>
             <div>
                 <label>Confirm Password</label>
-                <input type="password" name="confirmPassword" value={data.confirmPassword} onChange={changeHandler} />
+                <input type="password" name="confirmPassword" value={data.confirmPassword} onChange={changeHandler} onFocus={focusHandler} />
+                {errors.confirmPassword && touched.confirmPassword && <span>{errors.confirmPassword}</span>}
             </div>
             <div>
                 <label>I accepted terms of privacy policy</label>
-                <input type="checkbox" name="isAccepted" value={data.isAccepted} onChange={changeHandler} />
+                <input type="checkbox" name="isAccepted" value={data.isAccepted} onChange={changeHandler} onFocus={focusHandler} />
+                {errors.isAccepted && touched.isAccepted && <span>{errors.isAccepted}</span>}
             </div>
             <div>
                 <a href='#'>Login</a>
                 <button type='submit'>Sign up</button>
             </div>
-        </div>
+        </form>
     );
 };
 
---
+++ src/components/validate.js
@@ -0,0 +1,43 @@
+export const validate = (data)=>{
+    const errors={}
+    if(!data.name.trim()){
+        errors.name='Username is required'
+    }
+    else{
+        delete errors.name;
+    }
+    if(!data.email){
+        errors.email='Email is required';
+    }
+    else if(!/\S+@\S+\.\S+/.test(data.email)){
+        errors.email='Email address is invalid';
+    }
+    else{
+        delete errors.email;
+    }
+    if(!data.password){
+        errors.password='Password is required';
+    }
+    else if(data.password.length < 6){
+        errors.password='Password needs tp be 6 character  or more';
+    }
+    else{
+        delete errors.password;
+    }
+    if(!data.confirmPassword){
+        errors.confirmPassword='Confirm password';
+    }
+    else if(data.confirmPassword !== data.password){
+        errors.confirmPassword='Password do not match';
+    }
+    else{
+        delete errors.confirmPassword;
+    }
+    if(data.isAccepted){
+        delete errors.isAccepted;
+    }
+    else{
+        errors.isAccepted='Accept our regulation'
+    }
+    return errors;
+}
